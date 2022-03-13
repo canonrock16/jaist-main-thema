625841782e29262746270e9f18048c384c722541
canonrock16 <canonrock0401@gmail.com>
2022-03-13 20:59:45 +0900

    update

--- reproduce_gachi_less_synapse.py
+++ reproduce_gachi_less_synapse.py
@@ -63,7 +63,7 @@ R = 0.8  # ratio about excitory-inhibitory neurons
 
 # 各ニューロングループの生成
 # 30mvでスパイクが発生する。数値積分法はeuler
-P = NeuronGroup(n * neuron_group_count, model=eqs, threshold="v>30*mvolt", reset=reset, method="euler")
+P = NeuronGroup(n * neuron_group_count, model=eqs, threshold="v=>30*mvolt", reset=reset, method="euler")
 re = np.random.random(int(n * neuron_group_count))
 ri = np.random.random(round(n * neuron_group_count))
 
@@ -151,8 +151,8 @@ for edge in tqdm(list(G.edges())):
     # 各興奮性ニューロンが、他のニューロングループと三本の接続を持つので、接続する確率は3/1000
     Ce.connect(f"is_excitatory_pre and group_pre == {source_group} and group_post == {target_group}",p=0.003)
 
-# C.delay = "int(rand*2000)/100*ms"
-Ce.delay = "20*ms"
+Ce.delay = 'rand()*20*ms'
+seed()
 Ci.delay["not is_excitatory_pre"] = 1 * ms
 
 Ce.w = 6.0
@@ -202,7 +202,7 @@ time5 = time.time()
 print("次の100秒までにかかった時間", time5 - time4, "sec")
 
 # スタンドアローンモードへ
-device.build(directory="output", compile=True, run=True, debug=False)
+device.build(directory=f"output_{ws_model_beta}", compile=True, run=True, debug=False)
 
 # wを保存
 savedir = f"./results/pickle/{timestamp}"
@@ -231,7 +231,9 @@ for i in tqdm(range(neuron_group_count)):
 time6 = time.time()
 print("lap計算までにかかった時間", time6 - time5, "sec")
 
-pd.DataFrame(lap[0]).to_csv("test_before.csv", index=False)
+savedir = f"./results/csv/{timestamp}"
+os.makedirs(savedir,exist_ok=True)
+pd.DataFrame(lap[0]).to_csv(f"{savedir}/test_before_{ws_model_beta}.csv", index=False)
 
 
 # 全てのニューロングループに対してMSEの計算を行う
@@ -243,6 +245,6 @@ for i in range(neuron_group_count):
 time7 = time.time()
 print("mse計算までにかかった時間", time7 - time6, "sec")
 
-pd.DataFrame(results).to_csv("test.csv", index=False)
+pd.DataFrame(results).to_csv(f"{savedir}/test_{ws_model_beta}.csv", index=False)
 time8 = time.time()
 print("全体の処理時間", time8 - time0, "sec")
